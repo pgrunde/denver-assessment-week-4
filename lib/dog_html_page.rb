623fbea4744de005fad9435ab18f2eac980c10d2
Peter Grunde <peter.a.grunde@gmail.com>
2014-06-30 09:46:59 -0600

    added multiple options for list method

--- lib/dog_html_page.rb
+++ lib/dog_html_page.rb
@@ -11,16 +11,48 @@ class DogHTMLPage
     end
   end
 
-  def list
-      string = "<ul>"
-      @array_of_hashes.each do |hash|
-        string += "<li>" + hash[:name] + "</li>"
-      end
-      string += "</ul>"
-    if @array_of_hashes.length <= 0
-      string = ""
-    end
-      string
+  # CONFIRMED WORKING
+
+  # def list
+  #     string = "<ul>"
+  #     @array_of_hashes.each do |hash|
+  #       string += "<li>" + hash[:name] + "</li>"
+  #     end
+  #     string += "</ul>"
+  #   if @array_of_hashes.length <= 0
+  #     string = ""
+  #   end
+  #     string
+  # end
+
+  # rewrote it to be inside one if/else CONFIRMED WORKING
+
+  # def list
+  #   if @array_of_hashes.length <= 0
+  #     ""
+  #   else
+  #     var = "<ul>"
+  #     @array_of_hashes.each do |hash|
+  #       var += "<li>#{hash[:name]}</li>"
+  #     end
+  #     var + "</ul>"
+  #   end
+  # end
+
+  # attempt with .map and a wrapper CONFIRMED WORKING
+
+  def wrapper(content)
+    "<ul>#{content}</ul>"
   end
+
+  def list
+    if @array_of_hashes.length <= 0
+      ""
+    else
+      @array_of_hashes.map! { |hash| "<li>#{hash[:name]}</li>" }
+      wrapper(@array_of_hashes.join)
+    end
+  end
+
 
 end
